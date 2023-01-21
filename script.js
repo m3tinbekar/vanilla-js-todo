294a3106385a8b867ae69e63a45a207cbdf9dc62
m3tinbekar <metinb3kar@gmail.com>
2023-01-21 12:14:42 +0300

    feat: todo app crud added

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 let name;
 
-const loginButton = document.getElementById("login");
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
+const loginButton = document.getElementById("login");
+const addTodoButton = document.getElementById("addTodo");
+const addTask = document.getElementById("task");
+const dueDate = document.getElementById("dueDate");
+dueDate.valueAsDate = new Date();
+const checkButton = document.getElementsByClassName("todo-check");
+
+const modal = document.getElementById("createTask");
+const btn = document.getElementById("newTask");
+const span = document.getElementsByClassName("close")[0];
 
 const todoPage = document.querySelector(".container");
 todoPage.style.display = "none";
@@ -19,9 +51,80 @@ const login = () => {
 };
 loginButton.addEventListener("click", () => login());
 
-const modal = document.getElementById("createTask");
-const btn = document.getElementById("newTask");
-const span = document.getElementsByClassName("close")[0];
+const checkTodo = (username, date, index, status) => {
+  let todos = JSON.parse(localStorage.getItem(username));
+  todos[date][index] = { task: todos[date][index].task, done: !status };
+  localStorage.setItem(username, JSON.stringify(todos));
+  renderTodos(username);
+};
+
+const addTodo = (username) => {
+  const task = document.getElementById("task").value;
+  const dueDate = new Date(document.getElementById("dueDate").value);
+  const formattedDate = ` ${days[dueDate.getDay()]}, ${
+    months[dueDate.getMonth()]
+  } ${dueDate.getDate()}`;
+
+  let todos = JSON.parse(localStorage.getItem(username)) || {};
+  if (!todos[formattedDate]) {
+    todos[formattedDate] = [];
+  }
+  todos[formattedDate].push({ task: task, done: false });
+  localStorage.setItem(username, JSON.stringify(todos));
+  modal.style.display = "none";
+  renderTodos(username);
+  window.alert("Task Succesfully Added");
+};
+
+const deleteTodo = (username, date, index) => {
+  let todos = JSON.parse(localStorage.getItem(username));
+  todos[date].splice(index, 1);
+  localStorage.setItem(username, JSON.stringify(todos));
+  renderTodos(username);
+};
+
+const updateTodo = (username, date, index, task, done) => {
+  let todos = JSON.parse(localStorage.getItem(username));
+  todos[date][index] = { task: task, done: done };
+  localStorage.setItem(username, JSON.stringify(todos));
+  renderTodos(username);
+};
+
+const renderTodos = (username) => {
+  const todos = JSON.parse(localStorage.getItem(username)) || {};
+  const todoList = document.getElementById("todo-list");
+  todoList.innerHTML = "";
+  for (const date in todos) {
+    if (todos[date].length > 0) {
+      const todoListItem = document.createElement("li");
+      todoListItem.classList.add("todo-list-item");
+      todoListItem.innerHTML = `<div class="date" id="dateId"> <span class="date-btn"><i class="fas fa-calendar"></i></span> ${date}</div>${todos[
+        date
+      ]
+        .map((todo, index) => {
+          return `
+          
+          <div class="${
+            todo.done ? "todo-content is-active" : "todo-content"
+          }"> 
+          <button class="todo-check" onclick="checkTodo('${username}', '${date}', ${index}, ${
+            todo.done
+          })"> <i class="fas fa-check"></i>  </button>
+          <div class="todo-content-text"> ${todo.task} </div>
+          <div class="right-buttons"> 
+          <button class="todo-delete" onclick="deleteTodo('${username}', '${date}', ${index})"><i class="fas fa-trash-alt"></i></button>
+          <button class="todo-update" onclick="updateTodo('${username}', '${date}', ${index}, prompt('Enter new task'), ${
+            todo.done
+          })"><i class="fas fa-edit"></i></button></div></div>
+          `;
+        })
+        .join("")}`;
+      todoList.appendChild(todoListItem);
+    }
+  }
+  addTask.value = "";
+  dueDate.value = "";
+};
 
 btn.onclick = function () {
   modal.style.display = "block";
@@ -38,3 +141,5 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+addTodoButton.addEventListener("click", () => addTodo(name));
